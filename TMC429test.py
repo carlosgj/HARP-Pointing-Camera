bbfcb07e19c16e0e75f44bb0e7dde0efe30c89a4
Carlos Gross Jones <c.gross.jones@gmail.com>
2020-07-27 07:14:30 +0100

    TMC429 works with both motors

--- TMC429test.py
+++ TMC429test.py
@@ -36,18 +36,22 @@ def readReg(addr):
 for i in range(64):
     print i, ["0x%02x"%x for x in readReg(i)]
 
-print writeReg(52, [0, 0, 0x20]) #Set en_sd
-print writeReg(3, [0,0,50]) #Vmax
-print writeReg(6, [0, 0, 5]) #Amax
-print writeReg(2, [0, 0, 1]) #Vmin
-print writeReg(0, [0, 0, 0])
+print writeReg(52,[0, 0, 0x20]) #Set en_sd
+print writeReg(0x03, [0, 0, 30]) #Vmax
+print writeReg(0x06, [0, 0, 20]) #Amax
+print writeReg(0x02, [0, 0, 1]) #Vmin
+print writeReg(0x00, [0, 0, 0])
 
+print writeReg(0x13, [0, 0, 30])
+print writeReg(0x16, [0, 0, 20])
+print writeReg(0x12, [0, 0, 1])
+print writeReg(0x10, [0, 0, 0])
+sleep(1)
+
+val = 0
 while True:
     print readReg(1)
-    print "Going to max"
-    print writeReg(0, [0, 8, 255])
-    sleep(3)
-    print readReg(1)
-    print "Going to 0"
-    print writeReg(0, [0, 0, 1])
-    sleep(3)
+    print writeReg(0x00, [val >> 16, val >> 8, val])
+    print writeReg(0x10, [val >> 16, val >> 8, val])
+    val += 256
+    sleep(0.3)
